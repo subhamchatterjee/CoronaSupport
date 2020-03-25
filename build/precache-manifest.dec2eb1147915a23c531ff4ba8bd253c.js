377ddf2b79e9835e0a045f147d3fd68717a94974
Subham Chatterjee <subham.b.chatterjee@gmail.com>
2020-03-26 00:50:34 +0530

    Updated build

---
+++ build/precache-manifest.dec2eb1147915a23c531ff4ba8bd253c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fccbbd7ec70a873ea09a8d16b3749d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd118cca8cd5206baba9",
+    "url": "/static/css/2.22d4d4d7.chunk.css"
+  },
+  {
+    "revision": "bd118cca8cd5206baba9",
+    "url": "/static/js/2.453b73bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.453b73bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7416609d363bc3a4e3",
+    "url": "/static/js/main.a1cca272.chunk.js"
+  },
+  {
+    "revision": "82fc9d214b270878b132",
+    "url": "/static/js/runtime-main.43774ffc.js"
+  }
+]);
--- src/routes.js
+++ src/routes.js
@@ -15,10 +15,10 @@ export default class Routes extends Component {
       <Router history={history}>
         <Switch>
           <Redirect exact from="/" to="/maharashtra" />
-          <Route exact path="/add-material" component={AddMaterialPage} />
+          {/*<Route exact path="/add-material" component={AddMaterialPage} />
           <Route exact path="/edit-material/:materialId" component={AddMaterialPage} />
           <Route exact path="/manage-materials" component={ManageMaterialsPage} />
-          <Route exact path="/manage-material/:materialId" component={ManageSingleMaterialPage} />
+          <Route exact path="/manage-material/:materialId" component={ManageSingleMaterialPage} />*/}
           <Route exact path="/:state" component={LandingPage} />
           <Redirect path="*" to="/" />
         </Switch>
